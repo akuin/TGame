f214a6d15a588879d1b7028efc731f1efbb9e0ee
akuin <30168980+akuin@users.noreply.github.com>
2026-07-06 18:19:38 +0200

    brick color

--- breakout/bricks.gd
+++ breakout/bricks.gd
@@ -17,14 +17,29 @@ func _process(delta: float) -> void:
 	pass
 	
 
+var row_colors: Array[Color] = [
+	Color.RED,
+	Color.ORANGE,
+	Color.YELLOW,
+	Color.GREEN,
+	Color.BLUE,
+]
+
 func spawn_bricks():
 	for row in ROWS:
 		for col in COLS:
 			var brick = brick_scene.instantiate()
 			add_child(brick)
-
 			brick.position = Vector3(
 				(col - (COLS - 1) / 2.0) * SPACING_X,
 				-row * SPACING_Y,
 				0
 			)
+			apply_row_color(brick, row_colors[row % row_colors.size()])
+
+func apply_row_color(brick: Node, color: Color):
+	var mesh_instance := brick.get_node("MeshInstance3D") as MeshInstance3D
+	if mesh_instance:
+		var mat := StandardMaterial3D.new()
+		mat.albedo_color = color
+		mesh_instance.material_override = mat
