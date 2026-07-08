23a8c1d02294f68b23f3a21877791011ce59b083
akuin <30168980+akuin@users.noreply.github.com>
2026-07-08 17:27:56 +0200

    ball reset

--- breakout/ball.gd
+++ breakout/ball.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 
 const SPEED = 5
 const JUMP_VELOCITY = 4.5
+const START_POS := Vector3(0.0, 0.5, 0.0)
+const LOST_Y := -5.0
+
 
 func _ready():
 	velocity = Vector3(0,1,0) * SPEED
@@ -10,6 +13,9 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
+	if global_position.y < LOST_Y:
+		emit_signal("ball_lost")
+		_reset()
 	#velocity += get_gravity() * delta * SPEED
 	#velocity += Vector3(0,1,0) * delta * SPEED
 	var col = move_and_collide(velocity * delta)
@@ -33,3 +39,12 @@ func _physics_process(delta: float) -> void:
 			print("point")
 	
 	velocity = velocity.normalized() * SPEED
+
+func _reset() -> void:
+	# Snap back to start and relaunch
+	global_position = START_POS
+	velocity = Vector3.ZERO
+	_launch()
+
+func _launch() -> void:
+	velocity = Vector3(randf_range(-1.0, 1.0), 1.0, 0.0).normalized() * SPEED
---
+++ breakout/game.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var score = 0
+var lives = 0
+var state = 0
+
+#Restart later = `get_tree().reload_current_scene()`.
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
